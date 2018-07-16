32abb6277b6eb400f137250cc130bc0cd91b1fcd
Preety Kumari <preetydakshana@gmail.com>
2018-07-16 17:08:24 +0530

    update solution

--- aggregate.js
+++ aggregate.js
@@ -9,77 +9,49 @@ const readDataFile = function datafile(filePath) {
   });
 };
 
-function writeFile(output, data) {
+const write = function writeFile(output, data) {
   return new Promise((resolve, reject) => {
     fs.writeFile(output, data, (err) => {
       if (err === null) {
         resolve(data);
       } else {
-        reject(error);
+        reject(err);
       }
     });
   });
-}
+};
 
-const continentfile = 'continent.txt';
+const continentfile = 'continent.json';
 const output = './output/output.json';
 const aggregate = filePath => new Promise((resolve, reject) => {
   Promise.all([readDataFile(filePath), readDataFile(continentfile)]).then((values) => {
-    const csvdata = values[0]; // csv file data
-    const cc = values[1];// continent country data
-    const stringdata = csvdata.toString();
-    const arrayOne = stringdata.split('\n');
-    const header = arrayOne[0].split(',');
-    const noOfRow = arrayOne.length;
-    const noOfCol = header.length;
-    const countryfiledataobject = [];
-    const conticountry = [];
+    const fileData = values[0]; // csv file data
+    const mapper = JSON.parse(values[1]);// continent country data
+    const data = fileData.replace(/['"]+/g, '').split('\n');
+    const headerText = data[0].split(',');
+    const indexOfCountry = headerText.indexOf('Country Name');
+    const indexOfgdp = headerText.indexOf('GDP Billions (US Dollar) - 2012');
+    const indexOfPopulation = headerText.indexOf('Population (Millions) - 2012');
+    const aggregateresult = {};
 
-    for (let i = 1; i < noOfRow - 1; i += 1) {
-      const obj = {};
-      const myNewLine = arrayOne[i].split(',');
-
-      for (let j = 0; j < noOfCol; j += 1) {
-        const headerText = header[j].substring(1, header[j].length - 1);
-        const valueText = myNewLine[j].substring(1, myNewLine[j].length - 1);
-        obj[headerText] = valueText;
+    // console.log(mapperdata[cells[IndexOfCountryName]])
+    // console.log(data[1]);
+    data.forEach((element) => {
+      const row = element.split(',');
+      if (mapper[row[indexOfCountry]] !== undefined) {
+        if (aggregateresult[mapper[row[indexOfCountry]]] === undefined) {
+          aggregateresult[mapper[row[indexOfCountry]]] = {};
+          aggregateresult[mapper[row[indexOfCountry]]].GDP_2012 = parseFloat(row[indexOfgdp]);
+          aggregateresult[mapper[row[indexOfCountry]]].POPULATION_2012 = parseFloat(row[indexOfPopulation]);
+        } else {
+          aggregateresult[mapper[row[indexOfCountry]]].GDP_2012
+            += parseFloat(row[indexOfgdp]);
+          aggregateresult[mapper[row[indexOfCountry]]].POPULATION_2012
+            += parseFloat(row[indexOfPopulation]);
+        }
       }
-      countryfiledataobject.push(obj);
-    }
-    const ccrow = cc.split('\n');
-    const countryContinentMap = new Map();
-    let ccsplit;
-    for (let i = 0; i < ccrow.length; i += 1) {
-      ccsplit = ccrow[i].split(',');
-      ccsplit[1] = ccsplit[1].replace(/\r/g, '');
-      countryContinentMap.set(ccsplit[0], ccsplit[1]);
-    }
-    /*  countryContinentMap - (country - continent map from external data ,
-      countryfiledataobject - (object created from data file )) */
-    for (let i = 0; i < countryfiledataobject.length; i += 1) {
-      if (countryfiledataobject[i]['Country Name'] !== 'European Union') {
-        conticountry.push(countryContinentMap.get(countryfiledataobject[i]['Country Name']));
-      }
-    }
-
-    const continent = new Set(conticountry);
-    const resultData = {};
-
-    continent.forEach((cont) => {
-      resultData[cont] = {
-
-        GDP_2012: 0,
-        POPULATION_2012: 0,
-
-
-      };
     });
-    for (let i = 0; i < conticountry.length; i += 1) {
-      resultData[conticountry[i]].GDP_2012 += parseFloat(countryfiledataobject[i]['GDP Billions (US Dollar) - 2012']);
-
-      resultData[conticountry[i]].POPULATION_2012 += parseFloat(countryfiledataobject[i]['Population (Millions) - 2012']);
-    }
-    writeFile(output, JSON.stringify(resultData)).then(() => {
+    write(output, JSON.stringify(aggregateresult)).then(() => {
       resolve();
     }).catch((error) => {
       reject(error);
@@ -88,4 +60,5 @@ const aggregate = filePath => new Promise((resolve, reject) => {
     reject(error);
   });
 });
+aggregate('./data/datafile.csv');
 module.exports = aggregate;
